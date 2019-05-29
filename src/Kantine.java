b707580609b897342487107b3ca96d8c3111a371
Steven Woudstra <stevenwoudstra@gmail.com>
2019-05-29 10:30:20 +0200

    w2-opdacht5

--- src/Kantine.java
+++ src/Kantine.java
@@ -2,6 +2,7 @@ public class Kantine {
 
     private Kassa kassa;
     private KassaRij kassarij;
+    private KantineAanbod kantineAanbod;
 
     /**
      * Constructor
@@ -11,22 +12,35 @@ public class Kantine {
         kassa = new Kassa(kassarij);
     }
 
+    public KantineAanbod getKantineAanbod() {
+        return kantineAanbod;
+    }
+
+    public void setKantineAanbod(KantineAanbod kantineAanbod) {
+        this.kantineAanbod = kantineAanbod;
+    }
+
     /**
      * In deze methode wordt een Persoon en Dienblad gemaakt
      * en aan elkaar gekoppeld. Maak twee Artikelen aan
      * en plaats deze op het dienblad. Tenslotte sluit de
      * Persoon zich aan bij de rij voor de kassa.
      */
-    public void loopPakSluitAan() {
-        Persoon jan = new Persoon(981263123, "Jan", "Klaas", 2, 5, 1990, 'm');
+    public void loopPakSluitAan(Dienblad dienblad, String[] artikelnamen) {
+
+        for (String artiekel : artikelnamen) {
+            dienblad.voegToe(kantineAanbod.getArtikel(artiekel));
+        }
+
+//        Persoon jan = new Persoon(981263123, "Jan", "Klaas", 2, 5, 1990, 'm');
 //        Persoon anja = new Persoon(567123456, "Anja", "Bakker", 4, 8, 2003, 'v');
-        Dienblad dienbladJan = new Dienblad(jan);
+//        Dienblad dienbladJan = new Dienblad(jan);
 //        Dienblad dienbladAnja = new Dienblad(anja);
-        Artikel soep = new Artikel ("Soep", 150);
-        Artikel toasti = new Artikel ("Toasti", 90);
-        dienbladJan.voegToe(soep);
-        dienbladJan.voegToe(toasti);
-        kassarij.sluitAchteraan(dienbladJan);
+//        Artikel soep = new Artikel ("Soep", 150);
+//        Artikel toasti = new Artikel ("Toasti", 90);
+//        dienbladJan.voegToe(soep);
+//        dienbladJan.voegToe(toasti);
+//        kassarij.sluitAchteraan(dienbladJan);
     }
 
     /**
